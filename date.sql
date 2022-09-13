4bac7a787ea04f26b365ba93502634c45b4e277a
Maarten Uijen <m.uijen@ajax.nl>
2022-09-13 13:43:41 +0200

    inititial commit

---
+++ date.sql
@@ -0,0 +1,39 @@
+SET DATEFIRST  7, -- 1 = Monday, 7 = Sunday
+    DATEFORMAT ymd, 
+    LANGUAGE   US_ENGLISH;
+-- assume the above is here in all subsequent code blocks.
+
+DECLARE @StartDate  date = '20220101';
+
+DECLARE @CutoffDate date = DATEADD(DAY, -1, DATEADD(YEAR, 30, @StartDate));
+
+;WITH seq(n) AS 
+(
+  SELECT 0 UNION ALL SELECT n + 1 FROM seq
+  WHERE n < DATEDIFF(DAY, @StartDate, @CutoffDate)
+),
+d(d) AS 
+(
+  SELECT DATEADD(DAY, n, @StartDate) FROM seq
+),
+src AS
+(
+  SELECT
+    TheDate         = CONVERT(date, d),
+    TheDay          = DATEPART(DAY,       d),
+    TheDayName      = DATENAME(WEEKDAY,   d),
+    TheWeek         = DATEPART(WEEK,      d),
+    TheISOWeek      = DATEPART(ISO_WEEK,  d),
+    TheDayOfWeek    = DATEPART(WEEKDAY,   d),
+    TheMonth        = DATEPART(MONTH,     d),
+    TheMonthName    = DATENAME(MONTH,     d),
+    TheQuarter      = DATEPART(Quarter,   d),
+    TheYear         = DATEPART(YEAR,      d),
+    TheFirstOfMonth = DATEFROMPARTS(YEAR(d), MONTH(d), 1),
+    TheLastOfYear   = DATEFROMPARTS(YEAR(d), 12, 31),
+    TheDayOfYear    = DATEPART(DAYOFYEAR, d)
+  FROM d
+)
+SELECT * FROM src
+  ORDER BY TheDate
+  OPTION (MAXRECURSION 0);
